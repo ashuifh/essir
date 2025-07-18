041bf6faaf9ee8f49817a3121cf02cb0776aef6d
Ayush kalakoti <111656275+ashuifh@users.noreply.github.com>
2025-07-19 02:11:38 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -11,8 +11,9 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 const allowedOrigins = [
-  'http://localhost:5173',
+  'https://essir-1h5o.vercel.app'
 ];
+
 
 app.use(cors({
   origin: function (origin, callback) {
